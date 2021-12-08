ad58cd305c0d4c35f91aa117027184504b4369f5
Gavin Marler <gavin.marler17@gmail.com>
2021-12-07 19:42:36 -0700

    Initial Commit

---
+++ Omega/OmegaApp.swift
@@ -0,0 +1,17 @@
+//
+//  OmegaApp.swift
+//  Omega
+//
+//  Created by Gavin Marler on 12/7/21.
+//
+
+import SwiftUI
+
+@main
+struct OmegaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
